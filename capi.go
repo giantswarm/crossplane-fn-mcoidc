75038e22f3c93f6e901daf24a6ab5131a146b835
Pau Rosello <pau@giantswarm.io>
2025-09-09 12:18:22 +0200

    discover aws providerconfigs

--- capi.go
+++ capi.go
@@ -2,21 +2,76 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
+
+	kclient "github.com/giantswarm/xfnlib/pkg/auth/kubernetes"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	"github.com/giantswarm/xfnlib/pkg/composite"
 )
 
 func (f *Function) DiscoverAccounts(patchTo string, composed *composite.Composition) error {
 
-	type AccountInfo struct {
-		AccountID string `json:"accountId"`
-		RoleName  string `json:"roleName"`
+	client, err := kclient.Client()
+	if err != nil {
+		return err
 	}
 
-	v := []AccountInfo{
-		// TODO: Populate with actual account discovery logic
+	type AccountInfo struct {
+		AccountID         string `json:"accountId"`
+		RoleName          string `json:"roleName"`
+		ProviderConfigRef string `json:"providerConfigRef"`
+	}
+
+	// Get all ProviderConfig resources
+	providerConfigs := &unstructured.UnstructuredList{}
+	providerConfigs.SetAPIVersion("aws.upbound.io/v1beta1")
+	providerConfigs.SetKind("ProviderConfig")
+
+	if err := client.List(context.Background(), providerConfigs); err != nil {
+		return fmt.Errorf("cannot list ProviderConfig resources: %w", err)
+	}
+
+	// Track unique roleARNs to avoid duplicates
+	seenRoleARNs := make(map[string]bool)
+	var v []AccountInfo
+
+	for _, item := range providerConfigs.Items {
+		// Extract roleARN from the ProviderConfig spec
+		roleARN, found, err := unstructured.NestedString(item.Object, "spec", "credentials", "webIdentity", "roleARN")
+		if err != nil || !found || roleARN == "" {
+			f.log.Debug("cannot get roleARN from ProviderConfig", "error", err)
+			continue
+		}
+
+		// Skip if we've already seen this roleARN
+		if seenRoleARNs[roleARN] {
+			continue
+		}
+		seenRoleARNs[roleARN] = true
+
+		// Extract account ID from roleARN (format: arn:aws:iam::ACCOUNT_ID:role/ROLE_NAME)
+		parts := strings.Split(roleARN, ":")
+		if len(parts) < 5 {
+			continue
+		}
+		accountID := parts[4]
+
+		// Extract role name from roleARN
+		roleParts := strings.Split(roleARN, "/")
+		if len(roleParts) < 2 {
+			continue
+		}
+		roleName := roleParts[len(roleParts)-1]
+
+		v = append(v, AccountInfo{
+			AccountID:         accountID,
+			RoleName:          roleName,
+			ProviderConfigRef: item.GetName(),
+		})
 	}
 
 	b := &bytes.Buffer{}
@@ -25,6 +80,6 @@ func (f *Function) DiscoverAccounts(patchTo string, composed *composite.Composit
 		return fmt.Errorf("cannot encode to JSON: %w", err)
 	}
 
-	err := f.patchFieldValueToObject(patchTo, b.Bytes(), composed.DesiredComposite.Resource)
+	err = f.patchFieldValueToObject(patchTo, b.Bytes(), composed.DesiredComposite.Resource)
 	return err
 }
